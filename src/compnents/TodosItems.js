31fd7d514d27aa70534b1263f680625d60b20d6b
StianOek <66445281+StianOek@users.noreply.github.com>
2021-10-15 12:37:03 +0200

    proparly usecase of remove items

---
+++ src/compnents/TodosItems.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const TodosItems = ({ listItems, removeItems }) => {
+    return (
+        <div>
+            <Ul>
+                <p style={{textAlign: "start"}}>Todo's to complete</p>
+                <Line></Line>
+                {listItems.map( (item, index) => {
+                    return (
+                        <ItemWrapper>
+                            <Li key={index}>{item.list}</Li>
+                            <span list={item.list} onClick={removeItems}>close</span>
+                        </ItemWrapper>
+                    )
+                })}
+                
+            </Ul>
+        </div>
+    )
+}
+
+export default TodosItems
+
+const Line = styled.div`
+    width: 100%;
+    height: 1px;
+    background: #fff;
+    margin-bottom: 10px;
+`;
+
+const ItemWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    
+    span {
+        font-size: 14px;
+        cursor: pointer;
+        transition: 0.3s ease;
+        :hover {
+            transform: scale(1.1);
+            color: #ececec;
+            transition: 0.3s ease;
+        }
+    }
+`;
+
+const Ul = styled.ul`
+    border: 1px solid #fff;
+    border-radius: 10px;
+    padding: 10px 40px;
+    p {
+        margin: 0;
+    }
+`;
+
+const Li = styled.li`
+    list-style-type: none;
+    text-align: start;
+    font-size: 16px;
+`;
